565be193bf96e162cc18984d17cd46266b1ffecd
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2022-12-04 01:28:55 +0600

    Sales
    Sales

--- Inventory/inventory-mern-frontend/src/APIRequest/SaleAPIRequest.js
+++ Inventory/inventory-mern-frontend/src/APIRequest/SaleAPIRequest.js
@@ -1,10 +1,11 @@
 import store from "../redux/store/store";
 import {HideLoader, ShowLoader} from "../redux/state-slice/settings-slice";
 import axios from "axios";
-import {ErrorToast} from "../helper/FormHelper";
+import {ErrorToast, SuccessToast} from "../helper/FormHelper";
 import {getToken} from "../helper/SessionHelper";
 import {SetCustomerDropDown, SetProductDropDown, SetSaleList, SetSaleListTotal} from "../redux/state-slice/sale-slice";
 import {BaseURL} from "../helper/config";
+import {ResetBrandFormValue} from "../redux/state-slice/brand-slice";
 const AxiosHeader={headers:{"token":getToken()}}
 
 export async function SaleListRequest(pageNo, perPage, searchKeyword) {
@@ -75,5 +76,30 @@ export async function ProductDropDownRequest() {
     catch (e) {
         ErrorToast("Something Went Wrong")
         store.dispatch(HideLoader())
+    }
+}
+
+
+export async function CreateSaleRequest(ParentBody,ChildsBody) {
+    try {
+
+        store.dispatch(ShowLoader())
+        let PostBody={"Parent":ParentBody, "Childs":ChildsBody}
+        let URL = BaseURL+"/CreateSales"
+        const result = await axios.post(URL,PostBody,AxiosHeader)
+        store.dispatch(HideLoader())
+        if (result.status === 200 && result.data['status'] === "success") {
+            SuccessToast("Request Successful");
+            return  true;
+        }
+        else {
+            ErrorToast("Request Fail ! Try Again")
+            return false;
+        }
+    }
+    catch (e) {
+        ErrorToast("Something Went Wrong")
+        store.dispatch(HideLoader())
+        return  false
     }
 }
